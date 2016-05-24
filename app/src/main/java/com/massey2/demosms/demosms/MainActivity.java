bdfac1f2bdb013817fdde93e4b776d3d08174ee8
arj1211 <thejoker1211@outlook.com>
2016-05-24 17:02:55 -0400

    Added saving functionality when app process killed. Added "Add Msg/#" buttons to add user-defined presets. Bug fixes.

--- app/src/main/java/com/massey2/demosms/demosms/MainActivity.java
+++ app/src/main/java/com/massey2/demosms/demosms/MainActivity.java
@@ -1,10 +1,13 @@
 package com.massey2.demosms.demosms;
 
 import android.Manifest;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.telephony.SmsManager;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -15,22 +18,23 @@ import android.widget.ListView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity
+{
     EditText msgEdit, numEdit;
-    Button sendBtn, addMsgButton;
-
+    Button sendBtn, addMsgButton, clearPrefsButton, addNumButton;
     ListView msgList, numList;
-
     ArrayList<String> messages = new ArrayList<>(), numbers = new ArrayList<>();
+    BaseAdapter numberAdapter, messageAdapter;
 
-    BaseAdapter numberAdapter;
-    BaseAdapter messageAdapter;
+    SharedPreferences preferences;
 
+    static final String MSGKEY = "preset_messages", NUMKEY = "preset_nums", MSGEDITKEY = "msg_edit", NUMEDITKEY = "num_edit", MSGDELIM = "&&&&", NUMDELIM = ",";
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
@@ -38,65 +42,133 @@ public class MainActivity extends AppCompatActivity {
         msgEdit = (EditText) findViewById(R.id.messageText);
         sendBtn = (Button) findViewById(R.id.sendButton);
         addMsgButton = (Button) findViewById(R.id.addMsgButton);
+        addNumButton = (Button) findViewById(R.id.addNumButton);
+        clearPrefsButton = (Button) findViewById(R.id.clearPrefsButton);
         msgList = (ListView) findViewById(R.id.messageList);
         numList = (ListView) findViewById(R.id.numberList);
+
+        //Restore prefs
+        preferences = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+        msgEdit.setText(preferences.getString(MSGEDITKEY, ""));
+        numEdit.setText(preferences.getString(NUMEDITKEY, ""));
+        String numStr = preferences.getString(NUMKEY, "");
+        String msgStr = preferences.getString(MSGKEY, "");
+        String[] numStrArr = numStr.split(NUMDELIM);
+        String[] msgStrArr = msgStr.split(MSGDELIM);
+        numbers = new ArrayList<>(Arrays.asList(numStrArr));
+        messages = new ArrayList<>(Arrays.asList(msgStrArr));
+
         numberAdapter = new ArrayAdapter<>(MainActivity.this, android.R.layout.simple_list_item_1, numbers);
         messageAdapter = new ArrayAdapter<>(MainActivity.this, android.R.layout.simple_list_item_1, messages);
 
-        messages.add("Stuck in traffic, will be late.");
-        messages.add("Picking up dinner.");
-        messages.add("Can't talk right now.");
-        messages.add("Will call back later.");
-        messages.add("Massey Hacks II was lit!.");
-
-        numbers.add("6477128595");
-        numbers.add("6477700458");
-        numbers.add("6476877805");
-
-
+        if (messages.size() < 2)
+        {
+            messages.add("Stuck in traffic, will be late.");
+            messages.add("Picking up dinner.");
+            messages.add("Can't talk right now.");
+            messages.add("Will call back later.");
+            messages.add("Massey Hacks II was lit!.");
+        }
+//        if (numbers.size() < 2)
+//        {
+//            numbers.add("6477128595");
+//            numbers.add("6477700458");
+//            numbers.add("6476877805");
+//        }
         msgList.setAdapter(messageAdapter);
         numList.setAdapter(numberAdapter);
 
-        msgList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        msgList.setOnItemClickListener(new AdapterView.OnItemClickListener()
+        {
             @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id)
+            {
                 msgEdit.setText(msgList.getItemAtPosition(position).toString());
             }
         });
-
-        numList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        numList.setOnItemClickListener(new AdapterView.OnItemClickListener()
+        {
             @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id)
+            {
                 numEdit.setText(numList.getItemAtPosition(position).toString());
             }
         });
-
-        sendBtn.setOnClickListener(new View.OnClickListener() {
+        sendBtn.setOnClickListener(new View.OnClickListener()
+        {
             @Override
-            public void onClick(View v) {
+            public void onClick(View v)
+            {
                 sendSMS();
             }
         });
-
-        addMsgButton.setOnClickListener(new View.OnClickListener() {
+        addMsgButton.setOnClickListener(new View.OnClickListener()
+        {
             @Override
-            public void onClick(View v) {
-
-                if (!msgEdit.getText().toString().trim().equals("")) {
+            public void onClick(View v)
+            {
+                if (!msgEdit.getText().toString().trim().equals(""))
+                {
                     messages.add(msgEdit.getText().toString().trim());
                     messageAdapter.notifyDataSetChanged();
                 }
             }
         });
-
+        addNumButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                if (!numEdit.getText().toString().trim().equals("") && numEdit.getText().toString().matches("^[0-9 ]+$"))
+                {
+                    numbers.add(numEdit.getText().toString().trim());
+                    numberAdapter.notifyDataSetChanged();
+                }
+            }
+        });
+        clearPrefsButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                Toast.makeText(getApplicationContext(), "cleared prefs", Toast.LENGTH_SHORT).show();
+                preferences = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+                SharedPreferences.Editor ed = preferences.edit();
+                ed.clear();
+                numEdit.setText("");
+                msgEdit.setText("");
+                numbers.clear();
+                messages.clear();
+                numberAdapter.notifyDataSetChanged();
+                messageAdapter.notifyDataSetChanged();
+                ed.apply();
+            }
+        });
     }
 
-    private void sendSMS() {
-        try {
+    @Override
+    protected void onPause()
+    {
+        super.onPause();
+        Toast.makeText(getApplicationContext(), "paused..", Toast.LENGTH_SHORT).show();
+        preferences = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+        SharedPreferences.Editor ed = preferences.edit();
+        ed.putString(NUMKEY, TextUtils.join(NUMDELIM, numbers));
+        ed.putString(MSGKEY, TextUtils.join(MSGDELIM, messages));
+        ed.putString(MSGEDITKEY, msgEdit.getText().toString());
+        ed.putString(NUMEDITKEY, numEdit.getText().toString());
+        ed.apply();
+    }
+
+    private void sendSMS()
+    {
+        try
+        {
             SmsManager smsManager = SmsManager.getDefault();
             smsManager.sendTextMessage(numEdit.getText().toString(), null, msgEdit.getText().toString(), null, null);
             Toast.makeText(getApplicationContext(), "SMS sent.", Toast.LENGTH_LONG).show();
-        } catch (Exception e) {
+        } catch (Exception e)
+        {
             ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.SEND_SMS}, 1);
             Toast.makeText(getApplicationContext(), "SMS failed, please try again.", Toast.LENGTH_LONG).show();
             e.printStackTrace();
